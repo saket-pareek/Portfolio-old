12595215f6342a668b82345f107182782a7384dc
saket pareek <saket.wdev@gmail.com>
2020-02-08 15:46:45 +0530

    initialize fullpage

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,7 @@
-
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		//options here
+		licenseKey: '87DA34AD-A7964146-8344D0AA-9D0833DA'
+		// sectionsColor: ['#f9f700', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000']
+	});
+});
